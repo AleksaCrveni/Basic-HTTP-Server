3578b3b0cf2a6191337ac01486b5af47926ffe0c
WickedyWick <crvenialeksa@gmail.com>
2026-05-01 19:34:47 +0200

    Add project files.

---
+++ Server/HTTPServer.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+  public class HTTPServer
+  {
+    public Socket _socket;
+    public string IP;
+    public int PORT;
+    public HTTPServer(string ip, int port) 
+    {
+      IP = ip;
+      PORT = port;
+      _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+      //IPAddress hostIp = (Dns.Resolve(IPAddress.Any.ToString())).AddressList[0];
+      IPAddress hostIP = (Dns.GetHostEntry(IP)).AddressList[0];
+      IPEndPoint ep = new IPEndPoint(hostIP, port);
+      _socket.Bind(ep);
+      
+    }
+    
+    public void Start()
+    {
+      try
+      {
+        _socket.Listen();
+        Process();
+      }
+      catch (SocketException ex)
+      {
+        Console.WriteLine($"Socket Exception #{ex.NativeErrorCode}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Exception: {ex.Message}");
+      }
+      finally
+      {
+        _socket.Close();
+      }
+    }
+
+    public void Process()
+    {
+      while (true)
+      {
+      }
+    }
+
+  }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,7 @@
+﻿using Server;
+int port = 42069;
+string IP = "127.0.0.1";
+HTTPServer server = new HTTPServer(IP, port);
+server.Start();
+
+Console.ReadKey();
